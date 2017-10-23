70a22916e9c6b12b38b660cb3d58d8728f6e7f1a
Robert Laws <roblaws@me.com>
2017-10-23 05:58:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
         @@cart << new_item
         resp.write "added #{new_item}"
       else
-        resp.write "Item not found"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
